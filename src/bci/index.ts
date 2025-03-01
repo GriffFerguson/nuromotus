2a50cc86b89bae51e0d7f48c3dca7a9be9ee1c6b
Griffin Ferguson <griffinferguson.us@gmail.com>
2025-03-01 12:45:12 -0600

    fix(codebase): tsc compiled incorrect dir structure

--- src/bci/index.ts
+++ src/bci/index.ts
@@ -20,9 +20,9 @@ Cortex.onmessage = (e) => {
     // initial query to detect connected headset
     if (data.id == "initial_query") connectHeadset(data);
     // after headset is connected, authorize Cortex session
-    else if (data.id == "connect_headset") authorizeCortex(data)
+    else if (data.id == "connect_headset") authorizeCortex(data);
     // after cortex is authorized, create a session with the token
-    else if (data.id == "authorize_cortex") createCortexSession(data)
+    else if (data.id == "authorize_cortex") createCortexSession(data);
     
     // TODO: implement proper connection to Motor Controller service
 }
