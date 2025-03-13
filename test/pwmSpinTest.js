28ca110c28a40ab9cf49fc76c180d65715a29619
Griffin Ferguson <griffinferguson.us@gmail.com>
2025-03-13 14:03:02 -0500

    test: correct GPIO outputs for correct PWM outputs

--- test/pwmSpinTest.js
+++ test/pwmSpinTest.js
@@ -9,7 +9,7 @@ const PWM0_Test = setInterval(() => {
     PWM0_Pulse += 50
     if (PWM0_Pulse > 2000) {
         console.log("PWM_0 test complete. Sending stop signal (1500 microseconds)");
-        Output.Left[0].servoWrite(1500);
+        Output.Right[0].servoWrite(1500);
         // Output.PWM_0[1].servoWrite(1500);
         console.log("PWM_0 stop signal sent, exiting test.")
         clearInterval(PWM0_Test);
@@ -17,7 +17,7 @@ const PWM0_Test = setInterval(() => {
         return;
     }
     console.log(`Sending pulse of ${PWM0_Pulse} microseconds (${((PWM0_Pulse - 1500) / 500) * 100}%)`);
-    Output.Left[0].servoWrite(PWM0_Pulse);
+    Output.Right[0].servoWrite(PWM0_Pulse);
     // Output.PWM_0[1].servoWrite(PWM0_Pulse);
 }, 1000)
 
@@ -29,14 +29,14 @@ function startPWM1Test() {
         PWM1_Pulse += 50
         if (PWM1_Pulse > 2000) {
             console.log("PWM_1 test complete. Sending stop signal (1500 microseconds)");
-            Output.Right[0].servoWrite(1500);
+            Output.Left[0].servoWrite(1500);
             // Output.PWM_1[1].servoWrite(1500);
             console.log("PWM_1 stop signal sent, exiting test.")
             clearInterval(PWM1_Test);
             return;
         }
         console.log(`Sending pulse of ${PWM1_Pulse} microseconds (${((PWM1_Pulse - 1500) / 500) * 100}%)`);
-        Output.Right[0].servoWrite(PWM1_Pulse);
+        Output.Left[0].servoWrite(PWM1_Pulse);
         // Output.PWM_1[1].servoWrite(PWM1_Pulse);
     }, 1000)
 }
